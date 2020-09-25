5c2a242879c91ccc270dec35ad3cc402a53330e4
aryan29 <nk28agra@gmail.com>
2020-09-25 15:11:41 +0530

    Some More models added

--- quizoo/quizer/models.py
+++ quizoo/quizer/models.py
@@ -1,21 +1,35 @@
 from django.db import models
 from django.contrib.auth.models import User
 # Create your models here.
+
+
 class Quiz(models.Model):
-    admin=models.OneToOneField(User,on_delete=models.CASCADE) #User who created this quiz
-    quiz_name=models.CharField(max_length=50) #Quiz Name
-    start_time = models.DateTimeField() #Quiz Start Time
-    end_time=models.DateTimeField() #Quiz End Time
+    # User who created this quiz
+    admin = models.OneToOneField(User, on_delete=models.CASCADE)
+    quiz_name = models.CharField(max_length=50)  # Quiz Name
+    start_time = models.DateTimeField()  # Quiz Start Time
+    end_time = models.DateTimeField()  # Quiz End Time
+
 
 class Questions(models.Model):
-    quiz=ForeignKey(Quiz,on_delete=models.CASCADE) # To which quiz this question belongs too
-    question_text=models.CharField(max_length=500) #Question Text
+    # To which quiz this question belongs too
+    quiz = ForeignKey(Quiz, on_delete=models.CASCADE)
+    question_text = models.CharField(max_length=500)  # Question Text
+
 
 class Options(models.Model):
-    question=models.ForeignKey(Questions,on_delete=models.CASCADE) #This is an Option of which Question
-    option=models.CharField(max_length=100) #Option Text
+    # This is an Option of which Question
+    question = models.ForeignKey(Questions, on_delete=models.CASCADE)
+    option = models.CharField(max_length=100)  # Option Text
+
 
 class CorrectOptions(models.Model):
-    question=models.ForeignKey(Questions,on_delete=models.CASCADE)
-    option=models.CharField(max_length=100)
+    question = models.ForeignKey(Questions, on_delete=models.CASCADE)
+    option = models.CharField(max_length=100)
 
+
+class UsersGivingTest(models.Model):
+    quiz = models.ForeignKey(Quiz, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    score = models.SmallIntegerField()
+    question_attempted = models.ManyToManyField(Questions)
