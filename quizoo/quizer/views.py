e41fa0baeadb01bb108f6e35f85f209d3a8198fa
aryan29 <nk28agra@gmail.com>
2020-09-26 16:39:31 +0530

    Questions Added Showing UP now

--- quizoo/quizer/views.py
+++ quizoo/quizer/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from .forms import CreatingQuizForm
-from .models import Quiz
+from .models import Quiz,Questions,Options,CorrectOptions
 from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
 # Create your views here.
 
 
@@ -27,23 +28,59 @@ def ShowAllQuizToBeHeld(request):
     print(li)
     return render(request,'show_quiz.html',{"list":li})
 
+@csrf_exempt
 def EditQuiz(request,id):
     quiz=Quiz.objects.get(pk=id)
     if(quiz.admin==request.user):
         #Get All questions of this quiz
-        questions=quiz.questions_set.all()
-        li=[]
-        for q in questions:
-            op=q.options_set.all()
-            corr=q.correctoptions_set.all()
-            di={
-                "question":q,
-                "options":op,
-                "correct_options":corr
-            }
-            li.append(di)
-        
-        return render(request,'edit_quiz.html',{"list":li})
+        if(request.method=="GET"):
+            questions=quiz.questions_set.all()
+            print(questions)
+            li=[]
+            for q in questions:
+                op=q.options_set.all()
+                corr=q.correctoptions_set.all()
+                print(op)
+                print(corr)
+                di={
+                    "question":q,
+                    "options":op,
+                    "correct_options":corr
+                }
+                li.append(di)
+            
+            return render(request,'edit_quiz.html',{"list":li})
+        elif(request.method=="POST"):
+            data=request.POST
+            print(data)
+            print(data['question_text'])
+            print(data.getlist('options[]'))
+            
+            #Add entry into db from post data
+            #and return the response to append it if added in db successfully
+            try:
+                q=Questions.objects.create(
+                    question_text=data['question_text'],
+                    quiz=quiz
+                    )
+                for option in data.getlist('options[]'):
+                    Options.objects.create(
+                        option=option,
+                        question=q
+                    )
+                for correct_option in data.getlist('correct_options[]'):
+                    CorrectOptions.objects.create(
+                    option=correct_option,
+                        question=q
+                    )
+                return HttpResponse(200)
+            except Exception as e:
+                print(e)
+                return HttpResponse(500)
+            
+                
+            
+            
     else:
         return HttpResponse(400)
     #Check if this person can edit this quiz or not
