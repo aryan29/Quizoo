73d20b80db7686221f8103c013e5a45cd1158fe9
aryan29 <nk28agra@gmail.com>
2020-09-29 20:07:59 +0530

    boarding work on the way

--- quizoo/quizer/views.py
+++ quizoo/quizer/views.py
@@ -13,20 +13,22 @@ from django.utils import timezone
 
 @login_required(login_url='/accounts/login/')
 def CreateQuizView(request):
+    print(request.method)
     if(request.method=='POST'):
         form=CreatingQuizForm(request.POST)
         if(form.is_valid()):
-            print(form)
             obj=form.save(commit=False)
             obj.admin=request.user
             obj.save()
-            return render(request, 'create_quiz.html',{'form':CreatingQuizForm()})
+            return redirect('/create')
         else:
             print("Getting Errors Dude")
             return render(request, 'create_quiz.html',{'form':form,'errors':form.errors})
     else:
         return render(request, 'create_quiz.html',{'form':CreatingQuizForm()})
 
+# def CreateQuizEnpoint(request):
+    
 def ShowAllQuizToBeHeld(request):
     li=Quiz.objects.filter(admin=request.user,start_time__gt=timezone.now()) #Quiz hasnt started yet
     return render(request,'show_quiz.html',{"list":li})
