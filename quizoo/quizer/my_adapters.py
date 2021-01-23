a791abff5e2c9ff510bc93537f3d1bfafacea9c5
Aryan Khandelwal <nk28agra@gmail.com>
2021-01-24 04:28:36 +0530

    Adding filter

---
+++ quizoo/quizer/my_adapters.py
@@ -0,0 +1,9 @@
+from allauth.socialaccount.adapter import DefaultSocialAccountAdapter
+from allauth.exceptions import ImmediateHttpResponse
+from django.shortcuts import render
+class MySocialAccount(DefaultSocialAccountAdapter):
+    def pre_social_login(self, request, sociallogin):
+        print("Coming to pre social login")
+        u = sociallogin.user
+        if not u.email.split('@')[1] == "bitmesra.ac.in":
+            raise ImmediateHttpResponse(render(None,'error.html'))
